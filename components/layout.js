48a98dd35685e46d8a6496fa626c06f831972abf
kimlin20011 <kimlin20011@gmail.com>
2021-09-13 00:04:36 +0900

    final

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
-const name = "Shun LIN";
+const name = "Shun Lin";
 export const siteTitle = "Shun Next.js Sample Website";
 
 export default function Layout({ children, home }) {
